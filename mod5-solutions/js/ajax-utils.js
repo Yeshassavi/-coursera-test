a47ad943437ae82ff80edb192302d2ae0da2b71c
Yeshassavi <83069423+Yeshassavi@users.noreply.github.com>
2021-05-26 23:35:34 +0530

    Update ajax-utils.js

--- mod5-solutions/js/ajax-utils.js
+++ mod5-solutions/js/ajax-utils.js
@@ -31,7 +31,7 @@ ajaxUtils.sendGetRequest =
                        isJsonResponse); 
       };
     request.open("GET", requestUrl, true);
-     // for POST only
+    request.send(null); // for POST only
   };
 
 
@@ -64,4 +64,5 @@ global.$ajaxUtils = ajaxUtils;
 
 
 })(window);
+
 
